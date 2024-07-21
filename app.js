1b1450f7d87dc3df24cfb3ce93e18e488f4cafee
SaraSaragozin <saralizeth0711@gmail.com>
2024-07-21 18:18:48 -0500

    Add CORS for conexion

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3014;
+
+app.use(cors({
+  origin: '*',
+  methods: 'GET,POST,PUT,DELETE',
+}));
 
 app.use(express.json());
 
